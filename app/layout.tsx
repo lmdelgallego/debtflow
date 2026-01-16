b48b6d2ff08754d3f1600a44ca16505c41344560
Luis Miguel Del Gallego Horta <luis.delgallego@licify.co>
2026-01-16 09:03:49 -0500

    feat(layout): remove Navbar import from layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { cn } from "@/lib/utils";
-import Navbar from "@/components/Navbar";
 import { AuthProvider } from "@/context/auth-context";
 import { ToastProvider } from "@/components/ui/Toast";
 
